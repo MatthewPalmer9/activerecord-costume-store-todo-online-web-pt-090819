3f66f68cb2312d6fd2f02827c899cb1e6aee0283
Matthew Ryan Palmer <matthewpalmer9@outlook.com>
2019-11-20 02:26:58 +0000

    Automatically backed up by Learn

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -1,16 +1,1 @@
 # Create your haunted_houses migration here
-
-class HauntedHouse
-  def change
-    create table :haunted_houses do |t|
-      t.string :name
-      t.string :location
-      t.string :theme
-      t.float :price
-      t.boolen :family_friendly
-      t.datetime :opening_date
-      t.datetime :closing_date
-      t.
-    end
-  end
-end
