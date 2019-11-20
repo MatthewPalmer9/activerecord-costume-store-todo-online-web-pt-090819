c6368c9650ceb3370796ec1e5203a8e9cf4105d1
Matthew Ryan Palmer <matthewpalmer9@outlook.com>
2019-11-20 03:01:20 +0000

    Automatically backed up by Learn

--- app/models/costume_store.rb
+++ app/models/costume_store.rb
@@ -1,15 +1,5 @@
 # Create your CostumeStore class here
 
 class CostumeStore
-  def change
-    create_table :costume_stores do |t|
-      t.text :name
-      t.string :location
-      t.integer :costume_intentory
-      t.integer :employees
-      t.boolean :still_in_business
-      t.datetime :opening_time
-      t.datetime :closing_time
-    end
-  end
+  
 end
