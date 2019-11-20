8eb567b825d4cb7443a3c8f8cbf6f44b68eda6e3
Matthew Ryan Palmer <matthewpalmer9@outlook.com>
2019-11-20 02:30:47 +0000

    Automatically backed up by Learn

--- app/models/costume.rb
+++ app/models/costume.rb
@@ -2,14 +2,5 @@
 # It should inherit from ActiveRecord::Base
 
 class Costume < ActiveRecord::Base
-  def change
-    create_table :costumes do |t|
-      t.string :name
-      t.float :price
-      t.string :size
-      t.string :image_url
-      t.datetime :created_at
-      t.datetime :updated_at
-    end
-  end
+  
 end
