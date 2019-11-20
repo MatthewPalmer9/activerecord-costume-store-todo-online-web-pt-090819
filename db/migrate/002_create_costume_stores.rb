59ead7eb2c042835eac8ab03b02efc0185baf49c
Matthew Ryan Palmer <matthewpalmer9@outlook.com>
2019-11-20 02:59:55 +0000

    Automatically backed up by Learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -3,6 +3,9 @@ class CreateCostumeStores
     create_table :costume_stores do |t|
       t.text :name
       t.text :location
+      t.integer :costume_intentory
+      t.integer :employees
+      t.boolean :still_in_business
       t.
     end
   end
